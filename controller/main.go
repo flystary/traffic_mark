720170bb6f3e6b086f11e285906873da070cda4c
liql <651360107@qq.com>
2026-05-15 18:35:49 +0800

    完善tc挂载逻辑

--- controller/main.go
+++ controller/main.go
@@ -43,7 +43,7 @@ func main() {
 		m.Pinning = ebpf.PinByName
 	}
 
-	var objs TrafficObjects
+	var objs TrafficMarkObjects
 	opts := ebpf.CollectionOptions{
 		Maps: ebpf.MapOptions{PinPath: bpfFSPath},
 	}
